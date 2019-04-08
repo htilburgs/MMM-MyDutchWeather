bd5b057adac86aa820f5768b431da257d08d0f5f
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-08 16:27:47 +0200

    Add option for only showing location and temperature

--- MMM-MyDutchWeather.js
+++ MMM-MyDutchWeather.js
@@ -14,11 +14,12 @@ Module.register('MMM-MyDutchWeather', {
 		longitude: null,				// Longitude of your city
 		apiKey: null,					// API Key - Get for free at http://weerlive.nl/api/toegang/index.php
 		showIcons: true,				// Display Icons or Text
+		showExtra: false,				// Display additional weather information
 		maxWidth: "500px",				// Max width wrapper
-		animationSpeed: 1000, 			// fade in and out speed
+		animationSpeed: 1000, 				// fade in and out speed
 		initialLoadDelay: 1000,
 		retryDelay: 2500,
-		updateInterval: 10 * 60 * 1000	// every 10 minutes
+		updateInterval: 10 * 60 * 1000			// Every 10 minutes
 	},
 		
 	// Define stylesheet
@@ -39,7 +40,7 @@ Module.register('MMM-MyDutchWeather', {
 		return false;
 	},
 	
-	start: function() {
+	start: function () {
 		Log.info("Starting module: " + this.name);
 		requiresVersion: "2.1.0",	
 			
@@ -49,7 +50,7 @@ Module.register('MMM-MyDutchWeather', {
 		this.scheduleUpdate();       	// <-- When the module updates (see below)
 	},
 
-	getDom: function() {
+	getDom: function () {
 		
 		// creating the table
 		var table = document.createElement("table");
@@ -109,144 +110,147 @@ Module.register('MMM-MyDutchWeather', {
 		HuidigRow.appendChild(HuidigCell);
 		table.appendChild(HuidigRow);		
 		
-		var spacer = document.createElement("span");
-		spacer.innerHTML = "&nbsp;";
-		table.appendChild(spacer);
-		
-		var NeerslagRow = document.createElement("tr");
-		NeerslagRow.className = "neerslag-row";
-		
-		var NeerslagTextCell = document.createElement("td");
-		NeerslagTextCell.className = "small neerslagtextcell";
-		NeerslagTextCell.innerHTML = this.config.showIcons == false ? "Neerslagkans" : '<i class="wi wi-umbrella"></i>';	
-		NeerslagRow.appendChild(NeerslagTextCell);
-		table.appendChild(NeerslagRow);
-			
-		var NeerslagDataCell = document.createElement("td");
-		NeerslagDataCell.className = "small neerslagdatacell";
-		NeerslagDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.d0neerslag + " %" : MWB.d0neerslag + " %";
-		NeerslagRow.appendChild(NeerslagDataCell);
-		table.appendChild(NeerslagRow);			
-		
-		var ZonRow = document.createElement("tr");
-		ZonRow.className = "zon-row";
-		
-		var ZonTextCell = document.createElement("td");
-		ZonTextCell.className = "small zontextcell";
-		ZonTextCell.innerHTML = this.config.showIcons == false ? "Zonkans" : '<i class="wi wi-day-sunny"></i>';
-		ZonRow.appendChild(ZonTextCell);
-		table.appendChild(ZonRow);
-			
-		var ZonDataCell = document.createElement("td");
-		ZonDataCell.className = "small zondatacell";
-		ZonDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.d0zon + " %" : MWB.d0zon + " %";
-		ZonRow.appendChild(ZonDataCell);
-		table.appendChild(ZonRow);					
-		
-		var WindrichtingRow = document.createElement("tr");
-		WindrichtingRow.className = "windrichting-row";
-		
-		var WindrTextCell = document.createElement("td");
-		WindrTextCell.className = "small windrtextcell";
-		WindrTextCell.innerHTML = this.config.showIcons == false ? "Windrichting" : '<i class="wi wi-wind-direction"></i>';
-		WindrichtingRow.appendChild(WindrTextCell);
-		table.appendChild(WindrichtingRow);
-			
-		var WindrDataCell = document.createElement("td");
-		WindrDataCell.className = "small windrdatacell";
-		WindrDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.windr : MWB.windr;
-		WindrichtingRow.appendChild(WindrDataCell);
-		table.appendChild(WindrichtingRow);		
+		if (this.config.showExtra != false) {
+			var spacer = document.createElement("span");
+			spacer.innerHTML = "&nbsp;";
+			table.appendChild(spacer);
 
-		var WindkrachtRow = document.createElement("tr");
-		WindkrachtRow.className = "windkracht-row";
+			var NeerslagRow = document.createElement("tr");
+			NeerslagRow.className = "neerslag-row";
 		
-		var WindsTextCell = document.createElement("td");
-		WindsTextCell.className = "small windstextcell";
-		WindsTextCell.innerHTML = this.config.showIcons == false ? "Windkracht" : '<i class="wi wi-strong-wind"></i>';
-		WindkrachtRow.appendChild(WindsTextCell);
-		table.appendChild(WindkrachtRow);
-			
-		var WindsDataCell = document.createElement("td");
-		WindsDataCell.className = "small windsdatacell";
-		WindsDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.winds + " Bft" : MWB.winds + " Bft";
-		WindkrachtRow.appendChild(WindsDataCell);
-		table.appendChild(WindkrachtRow);		
+			var NeerslagTextCell = document.createElement("td");
+			NeerslagTextCell.className = "small neerslagtextcell";
+			NeerslagTextCell.innerHTML = this.config.showIcons == false ? "Neerslagkans" : '<i class="wi wi-umbrella"></i>';	
+			NeerslagRow.appendChild(NeerslagTextCell);
+			table.appendChild(NeerslagRow);
 
-		var LuchtdrukRow = document.createElement("tr");
-		LuchtdrukRow.className = "luchtdruk-row";
-		
-		var LuchtdTextCell = document.createElement("td");
-		LuchtdTextCell.className = "small luchtdtextcell";
-		LuchtdTextCell.innerHTML = this.config.showIcons == false ? "Luchtdruk" : '<i class="wi wi-barometer"></i>';
-		LuchtdrukRow.appendChild(LuchtdTextCell);
-		table.appendChild(LuchtdrukRow);
-			
-		var LuchtdDataCell = document.createElement("td");
-		LuchtdDataCell.className = "small luchtddatacell";
-		LuchtdDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.luchtd : MWB.luchtd;
-		LuchtdrukRow.appendChild(LuchtdDataCell);
-		table.appendChild(LuchtdrukRow);	
+			var NeerslagDataCell = document.createElement("td");
+			NeerslagDataCell.className = "small neerslagdatacell";
+			NeerslagDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.d0neerslag + " %" : MWB.d0neerslag + " %";
+			NeerslagRow.appendChild(NeerslagDataCell);
+			table.appendChild(NeerslagRow);			
 
-		var ZichtRow = document.createElement("tr");
-		ZichtRow.className = "zicht-row";
-		
-		var ZichtTextCell = document.createElement("td");
-		ZichtTextCell.className = "small zichttextcell";
-		ZichtTextCell.innerHTML = this.config.showIcons == false ? "Zicht" : '<i class="far fa-eye"></i>';
-		ZichtRow.appendChild(ZichtTextCell);
-		table.appendChild(ZichtRow);
+			var ZonRow = document.createElement("tr");
+			ZonRow.className = "zon-row";
+
+			var ZonTextCell = document.createElement("td");
+			ZonTextCell.className = "small zontextcell";
+			ZonTextCell.innerHTML = this.config.showIcons == false ? "Zonkans" : '<i class="wi wi-day-sunny"></i>';
+			ZonRow.appendChild(ZonTextCell);
+			table.appendChild(ZonRow);
+
+			var ZonDataCell = document.createElement("td");
+			ZonDataCell.className = "small zondatacell";
+			ZonDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.d0zon + " %" : MWB.d0zon + " %";
+			ZonRow.appendChild(ZonDataCell);
+			table.appendChild(ZonRow);					
+
+			var WindrichtingRow = document.createElement("tr");
+			WindrichtingRow.className = "windrichting-row";
+
+			var WindrTextCell = document.createElement("td");
+			WindrTextCell.className = "small windrtextcell";
+			WindrTextCell.innerHTML = this.config.showIcons == false ? "Windrichting" : '<i class="wi wi-wind-direction"></i>';
+			WindrichtingRow.appendChild(WindrTextCell);
+			table.appendChild(WindrichtingRow);
+
+			var WindrDataCell = document.createElement("td");
+			WindrDataCell.className = "small windrdatacell";
+			WindrDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.windr : MWB.windr;
+			WindrichtingRow.appendChild(WindrDataCell);
+			table.appendChild(WindrichtingRow);		
+
+			var WindkrachtRow = document.createElement("tr");
+			WindkrachtRow.className = "windkracht-row";
+
+			var WindsTextCell = document.createElement("td");
+			WindsTextCell.className = "small windstextcell";
+			WindsTextCell.innerHTML = this.config.showIcons == false ? "Windkracht" : '<i class="wi wi-strong-wind"></i>';
+			WindkrachtRow.appendChild(WindsTextCell);
+			table.appendChild(WindkrachtRow);
+
+			var WindsDataCell = document.createElement("td");
+			WindsDataCell.className = "small windsdatacell";
+			WindsDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.winds + " Bft" : MWB.winds + " Bft";
+			WindkrachtRow.appendChild(WindsDataCell);
+			table.appendChild(WindkrachtRow);		
+
+			var LuchtdrukRow = document.createElement("tr");
+			LuchtdrukRow.className = "luchtdruk-row";
+
+			var LuchtdTextCell = document.createElement("td");
+			LuchtdTextCell.className = "small luchtdtextcell";
+			LuchtdTextCell.innerHTML = this.config.showIcons == false ? "Luchtdruk" : '<i class="wi wi-barometer"></i>';
+			LuchtdrukRow.appendChild(LuchtdTextCell);
+			table.appendChild(LuchtdrukRow);
+
+			var LuchtdDataCell = document.createElement("td");
+			LuchtdDataCell.className = "small luchtddatacell";
+			LuchtdDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.luchtd : MWB.luchtd;
+			LuchtdrukRow.appendChild(LuchtdDataCell);
+			table.appendChild(LuchtdrukRow);	
+
+			var ZichtRow = document.createElement("tr");
+			ZichtRow.className = "zicht-row";
+
+			var ZichtTextCell = document.createElement("td");
+			ZichtTextCell.className = "small zichttextcell";
+			ZichtTextCell.innerHTML = this.config.showIcons == false ? "Zicht" : '<i class="far fa-eye"></i>';
+			ZichtRow.appendChild(ZichtTextCell);
+			table.appendChild(ZichtRow);
+
+			var ZichtDataCell = document.createElement("td");
+			ZichtDataCell.className = "small zichtdatacell";
+			ZichtDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.zicht + " KM" : MWB.zicht + " KM";
+			ZichtRow.appendChild(ZichtDataCell);
+			table.appendChild(ZichtRow);
+
+			var LuchtvochtigheidRow = document.createElement("tr");
+			LuchtvochtigheidRow.classname = "luchtvochtigheid-row";
+
+			var LuchtvTextCell = document.createElement("td");
+			LuchtvTextCell.className = "small luchtvtextcell";
+			LuchtvTextCell.innerHTML = this.config.showIcons == false ? "Luchtvochtigheid" : '<i class="wi wi-humidity"></i>';
+			LuchtvochtigheidRow.appendChild(LuchtvTextCell);
+			table.appendChild(LuchtvochtigheidRow);
+
+			var LuchtvDataCell = document.createElement("td");
+			LuchtvDataCell.className = "small luchtvdatacell";
+			LuchtvDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.lv + " %" : MWB.lv + " %";
+			LuchtvochtigheidRow.appendChild(LuchtvDataCell);
+			table.appendChild(LuchtvochtigheidRow);
+
+			var ZonopRow = document.createElement("tr");
+			ZonopRow.className = "zonop-row";
+
+			var ZonopTextCell = document.createElement("td");
+			ZonopTextCell.className = "small zonoptextcell";
+			ZonopTextCell.innerHTML = this.config.showIcons == false ? "Zonopkomst" : '<i class="wi wi-sunrise"></i>';
+			ZonopRow.appendChild(ZonopTextCell);
+			table.appendChild(ZonopRow);
+
+			var ZonopDataCell = document.createElement("td");
+			ZonopDataCell.className = "small zonopdatacell";
+			ZonopDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.sup : MWB.sup;
+			ZonopRow.appendChild(ZonopDataCell);
+			table.appendChild(ZonopRow);	
+
+			var ZonOnderRow = document.createElement("tr");
+			ZonOnderRow.className = "zononder-row";
+
+			var ZonOnderTextCell = document.createElement("td");
+			ZonOnderTextCell.className = "small zondertextcell";
+			ZonOnderTextCell.innerHTML = this.config.showIcons == false ? "Zonsondergang" : '<i class="wi wi-sunset"></i>';
+			ZonOnderRow.appendChild(ZonOnderTextCell);
+			table.appendChild(ZonOnderRow);
+
+			var ZonOnderDataCell = document.createElement("td");
+			ZonOnderDataCell.className = "small zonderdatacell";
+			ZonOnderDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.sunder : MWB.sunder;
+			ZonOnderRow.appendChild(ZonOnderDataCell);
+			table.appendChild(ZonOnderRow);			
 			
-		var ZichtDataCell = document.createElement("td");
-		ZichtDataCell.className = "small zichtdatacell";
-		ZichtDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.zicht + " KM" : MWB.zicht + " KM";
-		ZichtRow.appendChild(ZichtDataCell);
-		table.appendChild(ZichtRow);
-		
-		var LuchtvochtigheidRow = document.createElement("tr");
-		LuchtvochtigheidRow.classname = "luchtvochtigheid-row";
-		
-		var LuchtvTextCell = document.createElement("td");
-		LuchtvTextCell.className = "small luchtvtextcell";
-		LuchtvTextCell.innerHTML = this.config.showIcons == false ? "Luchtvochtigheid" : '<i class="wi wi-humidity"></i>';
-		LuchtvochtigheidRow.appendChild(LuchtvTextCell);
-		table.appendChild(LuchtvochtigheidRow);
-			
-		var LuchtvDataCell = document.createElement("td");
-		LuchtvDataCell.className = "small luchtvdatacell";
-		LuchtvDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.lv + " %" : MWB.lv + " %";
-		LuchtvochtigheidRow.appendChild(LuchtvDataCell);
-		table.appendChild(LuchtvochtigheidRow);
-		
-		var ZonopRow = document.createElement("tr");
-		ZonopRow.className = "zonop-row";
-		
-		var ZonopTextCell = document.createElement("td");
-		ZonopTextCell.className = "small zonoptextcell";
-		ZonopTextCell.innerHTML = this.config.showIcons == false ? "Zonopkomst" : '<i class="wi wi-sunrise"></i>';
-		ZonopRow.appendChild(ZonopTextCell);
-		table.appendChild(ZonopRow);
-			
-		var ZonopDataCell = document.createElement("td");
-		ZonopDataCell.className = "small zonopdatacell";
-		ZonopDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.sup : MWB.sup;
-		ZonopRow.appendChild(ZonopDataCell);
-		table.appendChild(ZonopRow);	
-		
-		var ZonOnderRow = document.createElement("tr");
-		ZonOnderRow.className = "zononder-row";
-		
-		var ZonOnderTextCell = document.createElement("td");
-		ZonOnderTextCell.className = "small zondertextcell";
-		ZonOnderTextCell.innerHTML = this.config.showIcons == false ? "Zonsondergang" : '<i class="wi wi-sunset"></i>';
-		ZonOnderRow.appendChild(ZonOnderTextCell);
-		table.appendChild(ZonOnderRow);
-			
-		var ZonOnderDataCell = document.createElement("td");
-		ZonOnderDataCell.className = "small zonderdatacell";
-		ZonOnderDataCell.innerHTML = this.config.showIcons == false ? ": " + MWB.sunder : MWB.sunder;
-		ZonOnderRow.appendChild(ZonOnderDataCell);
-		table.appendChild(ZonOnderRow);			
+		}
 
 		var FooterRow = document.createElement("tr");
 		FooterRow.className = "footer";
