39a7e85506214738e1a380107ee20d31bbd537dd
Harm Tilburgs <htilburgs@live.com>
2024-11-12 10:49:28 +0100

    Update MMM-MyDutchWeather.js
    Add translations

--- MMM-MyDutchWeather.js
+++ MMM-MyDutchWeather.js
@@ -31,6 +31,14 @@ Module.register('MMM-MyDutchWeather', {
 	getScripts: function () {
 		return ["moment.js"];
 	},
+
+	// Define required translations.
+  	getTranslations: function () {
+    		return {
+      		en: "translations/en.json",
+      		nl: "translations/nl.json"
+    		}
+  	},
 	
 	start: function () {
 		Log.info("Starting module: " + this.name);
